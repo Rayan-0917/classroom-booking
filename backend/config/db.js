c16d43c21a54e51b1ae7d98b94e16cd16f872440
Rayan Halder <merayanhalder06@gmail.com>
2026-08-13 17:33:44 +0530

    deploying frontend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,10 @@
 const Pool=require("pg").Pool
 
 const pool=new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE
+    connectionString: process.env.DATABASE_URL,
+    ssl:{
+        rejectUnauthorized: false
+    }
 })
 
 pool.on("error", (err)=>{
